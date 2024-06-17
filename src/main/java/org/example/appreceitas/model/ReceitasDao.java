29377eefa09a3b42648d14aa2f68b520a2863d98
LeomanCassio <leoman.cassio@hotmail.com>
2024-06-16 21:05:15 -0300

    att..

--- src/main/java/org/example/appreceitas/model/ReceitasDao.java
+++ src/main/java/org/example/appreceitas/model/ReceitasDao.java
@@ -6,10 +6,30 @@ import javafx.scene.control.Button;
 import java.lang.reflect.AccessFlag;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ReceitasDao {
     private List<Receita> receitas = new ArrayList<>();
     private List<Receita> receitasFavoritas = new ArrayList<>();
+    public void addReceita(Receita receita) {
+        receitas.add(receita);
+    }
 
+    public List<Receita> getReceitas() {
+        return receitas;
+    }
+    public List<Receita> filterByCategoria(String categoria) {
+        return receitas.stream().filter(receita -> receita.getCategoria().equals(categoria)).collect(Collectors.toList());
+    }
 
+    public void addFavoritaReceita(Receita receita) {
+        if (receitasFavoritas.size() < 3 && !receitasFavoritas.contains(receita)) {
+            receitasFavoritas.add(receita);
+        }
+    }
+
+    public List<Receita> getReceitasFavoritas() {
+        return receitasFavoritas;
+    }
 }
+
